8e600817212ef392abe65627a8cc16e006d76756
kayama <mysterious.blue.star@gmail.com>
2024-11-28 11:32:24 +0900

    更新

--- novel/merge.py
+++ novel/merge.py
@@ -1,7 +1,16 @@
 import glob  
 
-PATH = "C:\\Users\\314\\Documents\\git\\PyTools\\novel\\kage3\\*.txt"
-OUTPUT = "C:\\Users\\314\\Documents\\git\\PyTools\\novel\\kage_3.txt"
+num = 3
+
+JS_TEMPLATE = """var key = "kage_{0}";
+var data = `# 陰の実力者になりたくて！ 第{0}章
+
+{1}
+`;""";
+
+PATH = "C:\\Users\\314\\Documents\\git\\PyTools\\novel\\kage{0}\\*.txt".format(num)
+OUTPUT = "C:\\Users\\314\\Documents\\git\\PyTools\\novel\\kage_{0}.txt".format(num)
+JS_FILE = "C:\\Users\\314\\Documents\\git\\PyTools\\novel\\kage_{0}.js".format(num)
 
 txt_files = glob.glob(PATH) ##拡張子が「*txt」だけのファイルを返す
 
@@ -16,3 +25,5 @@ for file in txt_files:
 with open(OUTPUT, "w", encoding="utf-8") as fw:
     fw.write(ret)
 
+with open(JS_FILE, "w", encoding="utf-8") as fw:
+    fw.write(JS_TEMPLATE.format(num, ret))
