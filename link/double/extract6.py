9b1a4e6e9e0a44d84e0dbb1c5fe9a0c7e82c2241
kayama <mysterious.blue.star@gmail.com>
2025-02-05 17:12:57 +0900

    更新

---
+++ link/double/extract6.py
@@ -0,0 +1,21 @@
+
+def main(filename):
+    print("■" + filename)
+    buf = ''
+    with open(filename, "r", encoding="utf-8") as f:
+        buf = f.read()
+    
+    lines = buf.split('\n')
+    
+    ret = ""
+    
+    for i in range(1, len(lines)-1):
+        #print(lines[i])
+        if not lines[i].endswith("1"):
+            ret += lines[i] + "\n"
+
+    with open("multi6.csv", "w", encoding="utf-8") as f:
+        f.write(ret)
+
+if __name__ == '__main__':
+    main("result6.csv")
