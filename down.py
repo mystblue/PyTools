35496586c9aa08ab918dab1f7c13cfb7c57b6f2e
kayama <mysterious.blue.star@gmail.com>
2025-09-05 18:19:37 +0900

    更新

--- down.py
+++ down.py
@@ -9,13 +9,17 @@ __version__ = '0.0.1'
 
 import urllib.request#, urllib2
 
-with open("test.html", "wb") as frp:
+with open("srwy53.html", "wb") as frp:
     #http://book-checker.appspot.com/dl?url=
     #f = urllib2.urlopen("http://book-checker.appspot.com/dl?url=https://mevius.5ch.net/test/read.cgi/gamerobo/1635337031/")
     #f = urllib.request.urlopen(url = "https://ds-can.com/srw30/system/birthday.html")
     opener = urllib.request.build_opener()
     opener.addheaders =  [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.30')]
-    f = opener.open("https://mevius.5ch.net/test/read.cgi/gamerobo/1635393192/")
+    
+    #url = "https://mevius.5ch.net/test/read.cgi/gamerobo/1756209337/"
+    #url = "https://mevius.5ch.net/test/read.cgi/gamerobo/1756275623/"
+    url = "https://mevius.5ch.net/test/read.cgi/gamerobo/1756308681"
+    f = opener.open(url)
     
     #headers = { "User-Agent" :  "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)" }
     #f = urllib.request.add_header('User-Agent', 'Mozilla/5.0').urlopen("https://mevius.5ch.net/test/read.cgi/gamerobo/1635393192/")
