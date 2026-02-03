96b216b082ea3a2146ae0338c19b3ec05ca57988
kayama <mysterious.blue.star@gmail.com>
2026-02-03 10:11:22 +0900

    更新

--- stock/attention2/konami_1h.py
+++ stock/attention2/konami_1h.py
@@ -4,7 +4,7 @@ import yfinance as yf
 
 def fetch_data(code, name):
     ticker = yf.Ticker(code + ".T")
-    data = ticker.history(interval="1h", period="2d")
+    data = ticker.history(interval="1h", period="1d")
     #data = yf.download(code + ".T", start='2026-01-30', end='2026-02-02')
     data.to_csv(name + "_1h.csv")
 
